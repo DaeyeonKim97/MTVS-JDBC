90762fc3cebe9e33a57c1012b560016c4f20aa66
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-07-11 16:48:31 +0900

    Separate Method

--- chap1-section1-connection/src/com/dykim/section2/template/Application.java
+++ chap1-section1-connection/src/com/dykim/section2/template/Application.java
@@ -1,10 +1,17 @@
 package com.dykim.section2.template;
 
+import java.sql.Connection;
+//import static com.dykim.section2.template.JDBCTemplate.*; // static import, 패키지/클래스 내의 메소드를 인스턴스 메소드를 바로 사용할 수 있게 함
+
+
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Connection con = JDBCTemplate.getConnection();
+		System.out.println(con);
+		
+		JDBCTemplate.close(con);
 	}
+	
 
 }
