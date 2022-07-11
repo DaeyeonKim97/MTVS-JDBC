a75a5fc2e4d1ddd8c5c10d9c96a6938d47c97f77
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-07-11 14:34:48 +0900

    Set Properties File

--- chap1-section1-connection/src/com/dykim/section01/connection/Connection1.java
+++ chap1-section1-connection/src/com/dykim/section01/connection/Connection1.java
@@ -1,7 +1,10 @@
 package com.dykim.section01.connection;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 
 public class Connection1 {
@@ -9,16 +12,29 @@ public class Connection1 {
 		Connection con = null; //데이터베이스 접속 정보를 저장하고 있는 객체
 		
 		
+		//환경변수 설정
+		String filePath = "src/com/dykim/section01/connection/jdbc-config.properties";
+		Properties prop = new Properties();
+		try {
+			prop.load(new FileReader(filePath));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		String oracleDriver = prop.getProperty("ORACLE_DRIVER");
+		String url = prop.getProperty("ORACLE_URL");
+		String user = prop.getProperty("ORACLE_USER");
+		String password = prop.getProperty("ORACLE_PWD");
+		
+		
 		//Driver 연결 
 		try {
 			// 사용할 드라이버 등록
-			Class.forName("oracle.jdbc.driver.OracleDriver"); 
+			Class.forName(oracleDriver); 
 			/*Class는 클래스에 대한 모든 메타정보를 가지고 있음 
 			 * 런타임 당시에 해당 코드를 만났을 때 동적으로 로드할 수 있도록 하는 메세지 -> Class.forname() */
 			
-			String url = "jdbc:oracle:thin:@mtvsdb_medium?TNS_ADMIN=/Users/dykim/keys/Wallet_MTVSdb";
-			String user = "ADMIN";
-			String password = "Rlaeodus1130";
+			
 			
 			//url, user, password 3가지 정보로 DB접속 가능한 연결 객체 생성
 			con = DriverManager.getConnection(url,user,password);
