cf7f561e3f99758b21fb52a4e7977bd882bf7de8
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-07-11 11:28:58 +0900

    Connect to Oracle Cloud

---
+++ chap1-section1-connection/src/com/dykim/section01/connection/Connection1.java
@@ -0,0 +1,42 @@
+package com.dykim.section01.connection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Connection1 {
+	public static void main(String[] args) {
+		Connection con = null; //데이터베이스 접속 정보를 저장하고 있는 객체
+		
+		
+		//Driver 연결 
+		try {
+			// 사용할 드라이버 등록
+			Class.forName("oracle.jdbc.driver.OracleDriver"); 
+			/*Class는 클래스에 대한 모든 메타정보를 가지고 있음 
+			 * 런타임 당시에 해당 코드를 만났을 때 동적으로 로드할 수 있도록 하는 메세지 -> Class.forname() */
+			
+			String url = "jdbc:oracle:thin:@mtvsdb_medium?TNS_ADMIN=/Users/dykim/keys/Wallet_MTVSdb";
+			String user = "ADMIN";
+			String password = "Rlaeodus1130";
+			
+			//url, user, password 3가지 정보로 DB접속 가능한 연결 객체 생성
+			con = DriverManager.getConnection(url,user,password);
+			
+			System.out.println("con : "+con);
+			
+		} catch (ClassNotFoundException e) { //해당 클래스가 없는 에러가 발생했을 경우
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close(); //마지막에 닫아줌 
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
